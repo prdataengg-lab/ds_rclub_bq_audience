80cf617b45f485bff106813c707fb3cae400f87e
neha.devi <neha.devi@pivotroots.com>
2026-05-22 17:12:56 +0530

    Initial commit

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ logging.basicConfig(
 )
 log = logging.getLogger(__name__)
 
-MEASUREMENT_ID      = os.environ["G-L14XN09NVP"]
-API_SECRET          = os.environ["gm_r2CY-SM--64FisrZWOQ"]
-BQ_PROJECT          = os.environ["dsgroup-havas-csa"]
-USE_VALIDATION_MODE = os.environ.get("VALIDATION_MODE", "false").lower() == "true"
-THROTTLE_SECONDS    = float(os.environ.get("THROTTLE_SECONDS", "0.05"))
+MEASUREMENT_ID      = "G-L14XN09NVP"
+API_SECRET          = "gm_r2CY-SM--64FisrZWOQ"
+BQ_PROJECT          = "dsgroup-havas-csa"
+USE_VALIDATION_MODE = ("VALIDATION_MODE", "false").lower() == "true"
+THROTTLE_SECONDS    = float("THROTTLE_SECONDS", "0.05")
 
 GA4_ENDPOINT       = "https://www.google-analytics.com/mp/collect"
 GA4_VALIDATION_URL = "https://www.google-analytics.com/debug/mp/collect"
